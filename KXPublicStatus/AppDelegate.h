7d5e73a9867d44bf47d76f45e9e0f84abef2346c
KongXiangX <760736325@qq.com>
2017-10-16 14:38:53 +0800

    Initial Commit

---
+++ KXPublicStatus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KXPublicStatus
+//
+//  Created by apple on 2017/10/16.
+//  Copyright © 2017年 KX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KXPublicStatus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KXPublicStatus
+//
+//  Created by apple on 2017/10/16.
+//  Copyright © 2017年 KX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
